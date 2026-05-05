5bc8e74b44bb23bfb33471b0f2b7b2e772219282
Vianney <vpl.pinault@gmail.com>
2026-05-06 01:13:29 +0200

    fix: add sw.js that self-unregisters to clear stale service worker

---
+++ apps/web/public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => {
+  self.registration.unregister().then(() => {
+    self.clients.matchAll({ type: "window" }).then((clients) => {
+      clients.forEach((c) => c.navigate(c.url));
+    });
+  });
+});
